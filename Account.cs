fcd31ef530f3b3bce2118a57094456447f72710f
Hunter Larsen <hunterlarsen09@gmail.com>
2022-08-19 01:32:49 -0700

    implemented test conditions for withdraw method

--- Account.cs
+++ Account.cs
@@ -34,10 +34,16 @@ namespace BankAccount
         /// </summary>
         /// <param name="amt">amount deposit</param>
         /// <exception cref="NotImplementedException"></exception>
-        public void Depost(double amt)
+        public double Depost(double amt)
         {
-            throw new NotImplementedException();
+            Balance += amt;
+            return Balance;
         }
+/// <summary>
+/// withdraws specified amount of money from account
+/// </summary>
+/// <param name="amt"></param>
+/// <exception cref="NotImplementedException">amount to be withdrawn</exception>
         public void Withdraw(double amt)
         {
             throw new NotImplementedException();
